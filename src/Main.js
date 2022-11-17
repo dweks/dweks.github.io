8319c82295bb99b4a42a20a44ccf5cf206987851
Blake Stephens <blak7@pdx.edu>
2022-11-16 21:41:30 -0800

    created basic layout components and started some styling for mockup

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,48 @@
 import "./Main.css";
-import me from "./images/me.png";
 
 function Main() {
   return (
-      <img src={me} alt="Picture of Blake" />
+    <div id="main">
+      <LeftColumn />
+      <RightColumn />
+    </div>
   );
+}
+
+function LeftColumn() {
+  return (
+    <div id="main-left-col">
+      <Home />
+      <CurrentNav />
+    </div>
+  );
+}
+
+function RightColumn() {
+  return (
+    <div id="main-right-col">
+      <MainNav />
+      <CurrentView />
+    </div>
+  );
+}
+
+function Home() {
+  return <div id="home-container">Blake Stephens</div>;
+}
+
+// Site navigation container
+function MainNav() {
+  return <div id="main-nav-container"></div>;
+}
+
+// Main content; where site links are loaded
+function CurrentView() {
+  return <div id="current-view-container"></div>;
+}
+// Left sidebar housing the current page title and page subsections
+function CurrentNav() {
+  return <div id="current-nav-container"></div>;
 }
 
 export default Main;
