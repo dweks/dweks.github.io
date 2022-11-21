bc8cb20eee19d73f8de2f3484368e4a9cb77c09a
Blake Stephens <blak7@pdx.edu>
2022-11-21 15:56:44 -0800

    put navigation in ul

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import {BrowserRouter} from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import Main from "./Main";
+import Main from "./main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
