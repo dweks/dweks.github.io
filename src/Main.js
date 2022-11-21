1982e0217b4baaacbf4d1598c712661194fca790
Blake Stephens <blak7@pdx.edu>
2022-11-20 20:14:07 -0800

    basic skeleton of site; navigation routers function basically, styling applied, content pages created

--- src/Main.js
+++ src/Main.js
@@ -1,48 +1,87 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+
 import "./Main.css";
+import profileSmall from "./images/me.png";
 
 function Main() {
   return (
     <div id="main">
-      <LeftColumn />
-      <RightColumn />
+      <div id="main-left-col">
+        <div id="home" className="bahn">
+          <img
+            src={profileSmall}
+            id="home-self"
+            alt="Picture of Blake Stephens"
+            height="50px"
+          />
+          <p role="portfolio owner">Blake Stephens</p>
+        </div>
+        <Nav />
+      </div>
+      <div id="main-right-col">
+        <ContentHeader />
+        <Content />
+      </div>
     </div>
   );
 }
 
-function LeftColumn() {
+function ContentHeader() {
   return (
-    <div id="main-left-col">
-      <Home />
-      <CurrentNav />
-    </div>
+    <header id="content-header" className="bahn">
+      <div id="red-sep"></div>
+      <Routes>
+        <Route path="/about" element={<p>ABOUT</p>} />
+        <Route path="/projects" element={<p>PROJECTS</p>} />
+        <Route path="/experience" element={<p>EXPERIENCE</p>} />
+        <Route path="/contact" element={<p>CONTACT</p>} />
+      </Routes>
+    </header>
   );
-}
-
-function RightColumn() {
-  return (
-    <div id="main-right-col">
-      <MainNav />
-      <CurrentView />
-    </div>
-  );
-}
-
-function Home() {
-  return <div id="home-container">Blake Stephens</div>;
-}
-
-// Site navigation container
-function MainNav() {
-  return <div id="main-nav-container"></div>;
 }
 
 // Main content; where site links are loaded
-function CurrentView() {
-  return <div id="current-view-container"></div>;
+function Content() {
+  return (
+    <main id="content" className="round-corner-lg">
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
+  );
 }
-// Left sidebar housing the current page title and page subsections
-function CurrentNav() {
-  return <div id="current-nav-container"></div>;
+
+function Nav() {
+  return (
+    <nav id="nav" className="round-corner-lg bahn">
+      <NavLink to={"/about"} className="nav-link">
+        {/* Should do something but doesn't; checkout youtube video*/}
+        {({ isActive }) => {
+          return isActive ? "> " : "";
+        }}
+        About
+      </NavLink>
+
+      <NavLink to={"/projects"} className="nav-link">
+        Projects
+      </NavLink>
+
+      <NavLink to={"/experience"} className="nav-link">
+        Experience
+      </NavLink>
+
+      <NavLink to={"/contact"} className="nav-link">
+        Contact
+      </NavLink>
+    </nav>
+  );
 }
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Main from './Main';
+import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Main from "./Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Main />
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
   </React.StrictMode>
 );
